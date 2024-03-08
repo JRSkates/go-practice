f28cf9af2d194ecbcd62ec26d33282e853dd88d5
JRSkates <skatesproduction@gmail.com>
2024-03-08 14:09:05 +0000

    Ascii art function

---
+++ ascii.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+  f "fmt"
+)
+
+func main() {
+  f.Println("  __      _")
+  f.Println("o'')}____//")
+  f.Println(" `_/      )")
+  f.Println(" (_(_/-(_/ ")
+  f.Println("           ")
+}
