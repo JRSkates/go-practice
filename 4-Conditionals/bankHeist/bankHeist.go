32259947e99aaf4a8abeb70601d49f06f8b10b88
JRSkates <skatesproduction@gmail.com>
2024-03-20 15:46:29 +0000

    bank heist conditional program created

---
+++ 4-Conditionals/bankHeist/bankHeist.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+  "fmt"
+  "math/rand"
+  "time"
+)
+
+func main() {
+  rand.Seed(time.Now().UnixNano())
+
+  isHeistOn := true
+  eludedGuards := rand.Intn(100)
+
+  if eludedGuards >= 50 {
+    fmt.Println("Looks like you've managed to make it past the guards. Good job, but remember, this is the first step.")
+  } else {
+    isHeistOn = false
+    fmt.Println("Plan a better disguise next time?")
+  }
+
+  openedVault := rand.Intn(100)
+
+  if openedVault >= 70 {
+    fmt.Println("Grab and GO!")
+  } else if isHeistOn {
+    isHeistOn = false
+    fmt.Println("We can't open the vault!")
+  }
+
+  leftSafely := rand.Intn(5)
+
+  if isHeistOn {
+    switch leftSafely {
+      case 0:
+        isHeistOn = false
+        fmt.Println("You have been caught by a guard!")
+      case 1:
+        isHeistOn = false
+        fmt.Println("When did they start raising dogs in vaults?!")
+      case 2:
+        isHeistOn = false
+        fmt.Println("Looks like this fingerprint scanner won't accept any fingerprints...")
+      case 3:
+        isHeistOn = false
+        fmt.Println("Did I even pack the burlap bags?")
+      default:
+        fmt.Println("Start the getaway car!")
+    }
+  }
+
+  if isHeistOn {
+    amtStolen := 10000 + rand.Intn(1000000)
+    fmt.Println("£", amtStolen, "not bad!")
+  }
+
+  fmt.Println(isHeistOn)
+}
