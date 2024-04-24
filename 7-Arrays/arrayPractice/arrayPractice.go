da3ab7b350e5f75b6f8aa7b16411d3a63e45da41
JRSkates <skatesproduction@gmail.com>
2024-04-24 23:35:02 +0100

    practice array created

--- 7-Arrays/arrayPractice/arrayPractice.go
+++ 7-Arrays/arrayPractice/arrayPractice.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main() {
-
+    var practiceArray [5]string
+    practiceArray = [5]string{"this", "is", "a", "practice", "array"}
+    fmt.Println(practiceArray)
 }
