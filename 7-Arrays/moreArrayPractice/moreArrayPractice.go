504f3ff961b703943a44a82cee31b858559237af
JRSkates <skatesproduction@gmail.com>
2024-04-26 18:15:07 +0100

    accessing individual elements of an array

--- 7-Arrays/moreArrayPractice/moreArrayPractice.go
+++ 7-Arrays/moreArrayPractice/moreArrayPractice.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-
+	// Declaring and initializing an array
+	numbers := [5]int{1, 2, 3, 4, 5}
+	
+	// Printing the array
+	fmt.Println("Array:", numbers)
+	
+	// Accessing individual elements of the array
+	fmt.Println("Element at index 2:", numbers[2])
 }
